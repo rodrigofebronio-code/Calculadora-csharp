9960ad582538eeaf8e31101e23ae9534e2eba44e
Rodrigo Febrônio <rodrigo.febronio@docente.senai.br>
2026-05-07 08:55:13 -0300

    Add files via upload

---
+++ soma.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número inteiro: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número inteiro: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int soma = numero1 + numero2;
+
+        Console.WriteLine("A soma é: " + soma);
+    }
+}
